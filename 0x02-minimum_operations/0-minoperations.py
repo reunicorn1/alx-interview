b591cb430d5b0dcdf9d0d54569b1b2b2ad03e61f
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-13 16:52:27 +0400

    0-minoperations.py - greedy

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,42 +9,31 @@ def minOperations(n):
     This function calculates the fewest number of operations needed
     to exactly n H characters in a text file using Copy, Paste
     operations.
-
     -----------------------
     Parameters:
     n (int)
-
     -----------------------
     Returns:
     the minimum number of operations
     """
-def minOperations(n):
-    '''Computes the fewest number of operations needed to result
-    in exactly n H characters.
-    '''
-    if not isinstance(n, int):
+    if type(n) is not int or n < 1:
         return 0
+
     ops_count = 0
     clipboard = 0
     done = 1
-    # print('H', end='')
+
     while done < n:
         if clipboard == 0:
-            # init (the first copy all and paste)
             clipboard = done
             done += clipboard
             ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n - done) % done == 0:
-            # copy all and paste
+        elif (n - done) % done == 0:
             clipboard = done
             done += clipboard
             ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
-            # paste
+        else:
             done += clipboard
             ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
+
     return ops_count
