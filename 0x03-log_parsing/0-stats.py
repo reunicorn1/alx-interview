029251203553e129bfe5994397dff6ed972e8c24
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-21 02:17:32 +0400

    0-stats.py - troubleshoot3

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -87,7 +87,7 @@ def main():
                 count += 1
             if (count % 10 == 0):
                 print_log()
-        print_log
+        print_log()
     except KeyboardInterrupt or EOFError as e:
         print_log()
 
