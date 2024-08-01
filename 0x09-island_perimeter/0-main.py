4cf6b4118218328cb6d2d7e56db5c99e5996788c
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-08-01 11:55:43 +0400

    0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+island_perimeter = __import__('0-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        []
+    ]
+    print(island_perimeter(grid))
